720455fded766f2df6881e3386d66201af0f4170
jm6243188-coder <jm6243188@gmail.com>
2026-06-04 08:54:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,37 @@ const pizzas = [
     nome: "Pizza de Calabresa",
     descricao: "Molho de tomate, mussarela, calabresa, cebola e orégano.",
     preco: "45,00",
-    imagem: "img/calabresa.jpg.png"
+    imagem: "imagem/calabresa.jpg.png"
+  },
+  {
+    nome: "Pizza de Mussarela",
+    descricao: "Molho de tomate, mussarela especial, tomate e orégano.",
+    preco: "42,00",
+    imagem: "imagem/mussarela.jpg.png"
+  },
+  {
+    nome: "Pizza Portuguesa",
+    descricao: "Mussarela, presunto, ovo, cebola, ervilha, tomate e orégano.",
+    preco: "48,00",
+    imagem: "imagem/portuguesa.jpg.png"
+  },
+  {
+    nome: "Pizza de Frango com Catupiry",
+    descricao: "Frango desfiado, catupiry, mussarela, milho e orégano.",
+    preco: "50,00",
+    imagem: "imagem/frango-catupiry.jpg.png"
+  },
+  {
+    nome: "Pizza Quatro Queijos",
+    descricao: "Mussarela, provolone, parmesão, catupiry e orégano.",
+    preco: "52,00",
+    imagem: "imagem/quatro-queijos.jpg.png"
+  },
+  {
+    nome: "Pizza Marguerita",
+    descricao: "Mussarela, tomate, manjericão, molho de tomate e orégano.",
+    preco: "46,00",
+    imagem: "imagem/marguerita.jpg.png"
   }
 ];
 
@@ -16,7 +46,7 @@ pizzas.forEach(function(pizza) {
   card.className = "card";
 
   card.innerHTML = `
-    <img src="${pizza.imagem}" alt="${pizza.nome}">
+    <img class="pizza-img" src="${pizza.imagem}" alt="${pizza.nome}">
     <div class="card-content">
       <h2>${pizza.nome}</h2>
       <p>${pizza.descricao}</p>
